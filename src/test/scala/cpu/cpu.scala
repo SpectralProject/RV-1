431f977b177fd8ee4239bcef4f1f1643e0f44fd7
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-01 00:10:15 +1100

    Updates

--- src/test/scala/cpu/cpu.scala
+++ src/test/scala/cpu/cpu.scala
@@ -1,3 +1,14 @@
-class TestCPU {
-  println("in testCPU")
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+import cpu.{CPU}
+
+class CPUTest extends AnyFlatSpec with ChiselScalatestTester {
+  // test class body here
+  behavior of "CPU"
+  it should "Initialise" in {
+    test(new CPU) { c => }
+  }
+
 }
