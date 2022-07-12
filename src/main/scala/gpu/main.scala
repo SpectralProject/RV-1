1d5d8ecade7e601ef12e810543224b87ab1def47
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-12 14:22:25 +1000

    Update main.scala

--- src/main/scala/gpu/main.scala
+++ src/main/scala/gpu/main.scala
@@ -40,8 +40,27 @@ class GPUCluster(n: Int) extends Module {
   // sm_list(idx).wen := true.B
 }
 
+// SIMD on an SM?
+// so you have a warp that contains 1 instruction
+// and 32 data units. Each unit prob like 32-bits or 64-bits
+// These data units are "logical threads" of data in VRAM prob
+// Which you can load into a physical reg within the SM first
+// may need up to 32 LD/ST units
+
+// Spectre PT (prototype)
+// use warps as well. Or kernels
+// 1 way scheduling
+// 1 queue for all warps to enter the GPU from the microcontroller/VRAM
+// each warp is executed in sequence or out of order if possible. But if isb/dsb is specified, make sure to execute in order
+// simple branch prediction and instruction fetch based on most recent request for data read/write and branch
+// hardly any cache. Maybe like 1KB L1-L2 built into the HDL for FPGA, scaled up if needed
+// "64 bit" addressing. Can address up to 64 bits of memory but is configured with 2GB VRAM by default. Uses virtual memory through a L1/L2 TLBs built into the SM and cluster. Should be configured by the CPU for something like vulkan or multiple apps
+
 class GPU extends Module {
+  // BUS to microcontroller (STM32)
+  // BUS TO VRAM (2GB/parameterised)
   val io = IO(new Bundle {})
+
   // clusters, or 'cores'
   val cluster_1 = Module(new GPUCluster(4))
   val cluster_2 = Module(new GPUCluster(4))
